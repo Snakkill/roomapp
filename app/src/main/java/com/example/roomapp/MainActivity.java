81bf1959270a677ec50437b622d07faa2ec770fd
Ayman <aymanmoubayed@gmail.com>
2022-02-23 16:59:47 -0600

    Firestore committing data now functional. (JSON docs not provided)

--- app/src/main/java/com/example/roomapp/MainActivity.java
+++ app/src/main/java/com/example/roomapp/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.roomapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = MainActivity.class.getSimpleName();
     TodoRoomDb todoRoomDb;
     TodoDao todoDao;
     EditText etTitle,etNotes,etAuthor;
     TextView dbresults;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         etTitle = findViewById(R.id.Input_title);
         etNotes = findViewById(R.id.Input_Note);
         etAuthor = findViewById(R.id.Input_author);
+
+
 
     }
 
@@ -47,7 +60,34 @@ public class MainActivity extends AppCompatActivity {
             case R.id.retreviebttn:
                 searchTodo(etTitle.getText().toString());
                 break;
+            case R.id.commitFBbttn:
+                firecommit();
+                break;
         }
+    }
+
+    private void firecommit() {
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     private void searchTodo(String SearchString) {
